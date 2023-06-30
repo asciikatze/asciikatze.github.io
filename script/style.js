de90d8bd41f39b1ceac8693a1b7e86f7b7c5de98
isti varga <131890314+asciikatze@users.noreply.github.com>
2023-06-30 10:41:31 +0200

    Update style.js

--- script/style.js
+++ script/style.js
@@ -1,7 +1,7 @@
 function getStylesheet() {
   var currentTime = new Date().getHours();
-  if (0 <= currentTime&&currentTime < 8) {document.write("<link rel='stylesheet' href='nacht.css' type='text/css'>");}
-  if (8 <= currentTime&&currentTime < 20) {document.write("<link rel='stylesheet' href='tag.css' type='text/css'>");}
-  if (20 <= currentTime&&currentTime < 24) {document.write("<link rel='stylesheet' href='nacht.css' type='text/css'>");}
+  if (0 <= currentTime&&currentTime < 8) {document.write("<link rel='stylesheet' href='/style/nacht.css' type='text/css'>");}
+  if (8 <= currentTime&&currentTime < 20) {document.write("<link rel='stylesheet' href='/style/tag.css' type='text/css'>");}
+  if (20 <= currentTime&&currentTime < 24) {document.write("<link rel='stylesheet' href='/style/nacht.css' type='text/css'>");}
 }
 getStylesheet();
