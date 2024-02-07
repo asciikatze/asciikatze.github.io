923219d5e80c5ac4dc010962b01beeb06696411a
isti varga <131890314+asciikatze@users.noreply.github.com>
2024-02-07 17:48:14 +0100

    Update line.js

--- script/line.js
+++ script/line.js
@@ -5,6 +5,7 @@ function getRandomstring() {
   string[2] = "lack the ability to safely participate in group ordering at restaurants";
   string[3] = "i'm sorry if i gave you the impression that i know stuff";
   string[4] = "not good with verbal expression in an immediate fashion";
+  string[5] = "get mad; dont make lemonade";
   var i = Math.floor(Math.random()*string.length);
   document.write(string[i]);
 }
