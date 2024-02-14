584fb98d31188a0c00d58894583eae51a74597a5
isti varga <131890314+asciikatze@users.noreply.github.com>
2024-02-14 12:17:17 +0100

    Update line.js

--- script/line.js
+++ script/line.js
@@ -2,10 +2,10 @@ function getRandomstring() {
   var string = new Array();
   string[0] = "i could shove an ice pick in my eye; i could eat some fish from last july";
   string[1] = "it's not that i have something to hide; i have nothing i want you to see";
-  string[2] = "lack the ability to safely participate in group ordering at restaurants";
-  string[3] = "i'm sorry if i gave you the impression that i know stuff";
-  string[4] = "not good with verbal expression in an immediate fashion";
-  string[5] = "get mad; dont make lemonade";
+  string[2] = "i knew who i was this morning; but i have changed a few times since then";
+  string[3] = "lack the ability to safely participate in group ordering at restaurants";
+  string[4] = "i'm sorry if i gave you the impression that i know stuff";
+  string[5] = "not good with verbal expression in an immediate fashion";
   var i = Math.floor(Math.random()*string.length);
   document.write(string[i]);
 }
