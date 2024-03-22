acb028770f54f04c4bdeeb134f33f11f7182396f
isti varga <131890314+asciikatze@users.noreply.github.com>
2024-03-22 12:55:59 +0100

    Update line.js

--- script/line.js
+++ script/line.js
@@ -4,7 +4,7 @@ function getRandomstring() {
   string[1] = "it's not that i have something to hide; i have nothing i want you to see";
   string[2] = "i knew who i was this morning; but i have changed a few times since then";
   string[3] = "lack the ability to safely participate in group ordering at restaurants";
-  string[4] = "just a reminder that; if you were lazy then you'd be having fun";
+  string[4] = "just a reminder that if you were lazy then you'd be having fun";
   string[5] = "i'm sorry if i gave you the impression that i know stuff";
   string[6] = "not good with verbal expression in an immediate fashion";
   var i = Math.floor(Math.random()*string.length);
