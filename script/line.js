d4cf26860a2fdd9a953db48df31248abf63e9de3
isti_varga <131890314+asciikatze@users.noreply.github.com>
2026-01-08 16:28:03 +0100

    Update ssh.html
    remove copy to clipboard option when javascript is bisabled

--- script/line.js
+++ script/line.js
@@ -1,13 +1,14 @@
 function getRandomstring() {
   var string = new Array();
-  string[0] = "it's not that i have something to hide; i have nothing i want you to see";
-  string[1] = "lack the ability to safely participate in group ordering at restaurants";
-  string[2] = "if the oppressed cannot access technology, then it's not revolutionary";
-  string[3] = "just a reminder that if you were lazy then you'd be having fun";
-  string[4] = "culture shouldn't exist only for those who can afford it";
-  string[5] = "i'm sorry if i gave you the impression that i know stuff";
-  string[6] = "not good with verbal expression in an immediate fashion";
-  string[7] = "easily undoable things are pretty great";
+  string[0] = "why should i bother reading something that nobody could be bothered to write";
+  string[1] = "it's not that i have something to hide; i have nothing i want you to see";
+  string[2] = "lack the ability to safely participate in group ordering at restaurants";
+  string[3] = "if the oppressed cannot access technology, then it's not revolutionary";
+  string[4] = "just a reminder that if you were lazy then you'd be having fun";
+  string[5] = "culture shouldn't exist only for those who can afford it";
+  string[6] = "i'm sorry if i gave you the impression that i know stuff";
+  string[7] = "not good with verbal expression in an immediate fashion";
+  string[8] = "easily undoable things are pretty great";
   var i = Math.floor(Math.random()*string.length);
   document.write(string[i]);
   }
