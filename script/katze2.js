1219aa68c71b60d383371a279b50b74d03a0ac3b
isti_varga <131890314+asciikatze@users.noreply.github.com>
2026-08-26 10:07:35 +0200

    2635c Update
    shrunk the code

--- script/katze2.js
+++ script/katze2.js
@@ -1,19 +1,15 @@
 document.write("&nbsp;&nbsp;/\\_/\\<br>>^,^<<br>&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;\\<br>&nbsp;&nbsp;(__)___");
 var elem = document.getElementById("idle");
-var inst = setInterval(getAnimation, 500);
+var inst = setInterval(getAnimation, 1000);
 var counter = 0;
 var frame = [
   "&nbsp;&nbsp;/\\_/\\<br>>^,^<<br>&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;\\<br>&nbsp;&nbsp;(__)__/",
   "&nbsp;&nbsp;/\\_/\\<br>>^,^<<br>&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;\\<br>&nbsp;&nbsp;(__)__/",
-  "&nbsp;&nbsp;/\\_/\\<br>>^,^<<br>&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;\\<br>&nbsp;&nbsp;(__)__/",
-  "&nbsp;&nbsp;/\\_/\\<br>>^,^<<br>&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;\\<br>&nbsp;&nbsp;(__)__/",
-  "&nbsp;&nbsp;/\\_/\\<br>>^,^<<br>&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;\\<br>&nbsp;&nbsp;(__)___",
-  "&nbsp;&nbsp;/\\_/\\<br>>^,^<<br>&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;\\<br>&nbsp;&nbsp;(__)___",
   "&nbsp;&nbsp;/\\_/\\<br>>^,^<<br>&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;\\<br>&nbsp;&nbsp;(__)___",
   "&nbsp;&nbsp;/\\_/\\<br>>^,^<<br>&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;\\<br>&nbsp;&nbsp;(__)___"
   ];
 function getAnimation() {
   elem.innerHTML = frame[counter];
   counter++;
-  if (counter >= frame.length) {counter = frame.length-8}
+  if (counter >= frame.length) {counter = frame.length-4}
   }
